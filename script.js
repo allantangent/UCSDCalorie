9a78284183a1ae5bc20e0a11ec8118b6d3f13b71
Allan Tan <43974058+allantangent@users.noreply.github.com>
2019-04-07 01:56:27 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,195 @@
-var food1 = { name: 'Cheeseburger', calories: '500', cost: '$69.69' }
-var food2 = { name: 'Quylan', calories: '500', cost: '$69.69' }
-var countries = [ food1, food2]
+var worx1 = { name: 'Buffalo Chicken Sandwich', calories: '1171', cost: '$6.50' }
+var worx2 = { name: 'Grilled Cheese Sandwich', calories: '425', cost: '$2.95' }
+var worx3 = { name: 'Ham Sandwich', calories: '491', cost: '$6.95' }
+var worx4 = { name: 'Roast Beef Sandwich', calories: '583', cost: '$6.95' }
+var worx5 = { name: 'Tuna Sandwich', calories: '631', cost: '$5.95' }
+var worx6 = { name: 'Turkey Sandwich', calories: '579', cost: '$5.95' }
+var worx7 = { name: 'Veggie Sandwich', calories: '491', cost: '$4.95' }
+var worx8 = { name: 'Veggie Sandwich w/ Hummus', calories: '632', cost: '$5.95' }
+var worx9 = { name: 'BYO Salad', calories: '1011', cost: '$6.50' }
+var worx10 = { name: 'BYO Wrap', calories: '964', cost: '$6.75' }
+var worx11 = { name: 'Buffalo Blue Salad', calories: '764', cost: '$5.95' }
+var worx12 = { name: 'California Wrap', calories: '936', cost: '$6.75' }
+var worx13 = { name: 'Chicken Caesar Salad', calories: '419', cost: '$6.25' }
+var worx14 = { name: 'Chicken Caesar Wrap', calories: '706', cost: '$6.75' }
+var worx15 = { name: 'Cobb Salad', calories: '849', cost: '$6.25' }
+var worx16 = { name: 'Mandarin Salad', calories: '628', cost: '$6.25' }
+var worx17 = { name: 'Sesame Chicken Wrap', calories: '888', cost: '$6.75' }
+var worx18 = { name: 'Southwest Salad', calories: '734', cost: '$6.25' }
+var worx19 = { name: 'BBQ Hawaiian Personal Pizza', calories: '912', cost: '$5.25' }
+var worx20 = { name: 'BYO Personal Pizza Supreme', calories: '1003', cost: '$5.95' }
+var worx21 = { name: 'Cheese Personal Pizza', calories: '1008', cost: '$4.95' }
+var worx22 = { name: 'Meat Lover\'s Personal Pizza', calories: '880', cost: '$5.25' }
+var worx23 = { name: 'Pepperoni Personal Pizza', calories: '1024', cost: '$5.25' }
+var worx24 = { name: 'Supreme Personal Pizza', calories: '1009', cost: '$5.25' }
+var worx25 = { name: 'Vegetarian Personal Pizza', calories: '926', cost: '$5.25' }
+var worx26 = { name: 'Kale and Chourico Soup', calories: '173', cost: '$3.50' }
+var worx27 = { name: 'Mediterranean Lentil Soup', calories: '161', cost: '$3.50' }
+var l64d1 = { name: 'Vegetable Springroll', calories: '61', cost: '$1.00' }
+var l64d2 = { name: 'Vegetable Potsticker', calories: '89', cost: '$1.00' }
+var l64d3 = { name: 'Asian Noodle Salad', calories: '160', cost: '$2.00' }
+var l64d4 = { name: 'Orange Chicken Plate', calories: '480', cost: '$6.95' }
+var l64d5 = { name: 'Hibachi Chicken Plate', calories: '470', cost: '$6.95' }
+var l64d6 = { name: 'Curry Tofu Plate', calories: '540', cost: '$6.95' }
+var l64d7 = { name: 'Beef & Broccoli Plate', calories: '393', cost: '$6.95' }
+var l64d8 = { name: 'Vegetarian Sandwich', calories: '684', cost: '$4.95' }
+var l64d9 = { name: 'Turkey Sandwich', calories: '808', cost: '$5.95' }
+var l64d10 = { name: 'Tuna Salad Sandwich', calories: '530', cost: '$5.95' }
+var l64d11 = { name: 'Roast Beef Sandwich', calories: '809', cost: '$6.95' }
+var l64d12 = { name: 'Jackfruit Salad Sandwich', calories: '664', cost: '$5.95' }
+var l64d13 = { name: 'Ham Sandwich', calories: '845', cost: '$6.95' }
+var l64d14 = { name: 'Roots 3 Bean Chili', calories: '204', cost: '$3.50' }
+var l64d15 = { name: 'Broccoli Cheddar Soup', calories: '264', cost: '$3.50' }
+var l64d16 = { name: 'Fresh Garden Bar', calories: '30', cost: '$0.39' }
+var l64d17 = { name: 'Vegetarian Bagel', calories: '483', cost: '$3.95' }
+var l64d18 = { name: 'Turkey Bagel', calories: '544', cost: '$5.95' }
+var l64d19 = { name: 'Sweet Potato & Kale Scramble', calories: '199', cost: '$2.25' }
+var l64d20 = { name: 'Smoked Salmon Eggs Benedict', calories: '343', cost: '$4.95' }
+var l64d21= { name: 'Salmon Bagel Bar', calories: '727', cost: '$6.95' }
+var l64d22 = { name: 'French Toast with Berries', calories: '476', cost: '$4.75' }
+var l64d23 = { name: 'Breakfast Hash Plate', calories: '308', cost: '$5.00' }
+var l64d24 = { name: 'Biscuit & Gravy with Egg', calories: '338', cost: '$1.95' }
+var l64d25 = { name: 'Avocado Toast', calories: '239', cost: '$3.00' }
+var l64d26 = { name: '64D Polenta & Poached Egg', calories: '590', cost: '$4.95' }
+//CAFE VENTANAS UNDERNEATH
+var lcafev1 = { name: 'Barnyard Burger', calories: '966', cost: '$6.95' }
+var lcafev2 = { name: 'Cheese Pizza', calories: '281', cost: '$2.25' }
+var lcafev3 = { name: 'Hawaiian Pizza', calories: '398', cost: '$2.50' }
+var lcafev4 = { name: 'Pepperoni Pizza', calories: '367', cost: '$2.50' }
+var lcafev5 = { name: 'Broccoli Cheddar Soup', calories: '264', cost: '$3.50' }
+var lcafev6 = { name: 'Roots 3 Bean Chili', calories: '204', cost: '$3.50' }
+var lcafev7 = { name: 'Turkey Corn Dog', calories: '402', cost: '$2.25' }
+var lcafev8 = { name: 'City Dish Roasted Chicken', calories: '610', cost: '$5.50' }
+var lcafev9 = { name: 'Penne with Spicy Marinara', calories: '391', cost: '$4.95' }
+var lcafev10 = { name: 'Basmati Rice', calories: '149', cost: '$1.50' }
+var lcafev11 = { name: 'Roasted Red Potatoes', calories: '142', cost: '$2.00' }
+var lcafev12 = { name: 'Soy Ginger Broccoli', calories: '135', cost: '2.00' }
+var lcafev13 = { name: 'California Burrito', calories: '943', cost: '$6.95' }
+var lcafev14 = { name: 'Carne Asada Fries', calories: '738', cost: '$6.95' }
+var lcafev15 = { name: 'Vegetable Potsticker', calories: '89', cost: '$1.00' }
+var lcafev16 = { name: 'Vegetable Springroll', calories: '61', cost: '$1.00' }
+var food10 = { name: 'Cheeseburger', calories: '579', cost: '$4.95' }
+var food20 = { name: 'Chicken Breast Sandwich', calories: '431', cost: '$5.25' }
+var food30 = { name: 'Chipotle Black Bean Burger ', calories: '465', cost: '$5.95' }
+var food40 = { name: 'Chixin Vegan Berger', calories: '736', cost: '$5.95' }
+var food50 = { name: 'Double Cheeseburger', calories: '885', cost: '$7.75' }
+var food60 = { name: 'Double Hamburger', calories: '825', cost: '$6.25' }
+var food70 = { name: 'Grilled Cheese Sandwich', calories: '675', cost: '$2.95' }
+var food80 = { name: 'Grilled Turkey Burger', calories: '515', cost: '$5.95' }
+var food90 = { name: 'Ham Bobcat Sandwich', calories: '496', cost: '$3.95' }
+var food100 = { name: 'Hamburger', calories: '528', cost: '$4.20' }
+var food110 = { name: 'Plant Based Burger', calories: '620', cost: '$5.95' }
+var food120 = { name: 'Salmon Sandwich', calories: '680', cost: '$8.25' }
+var food130 = { name: 'Crispy Onion Rings', calories: '524', cost: '$3.50' }
+var food140 = { name: 'French Fries', calories: '422', cost: '$1.75' }
+var food150 = { name: 'Fried Chicken Fritters', calories: '676', cost: '$4.50' }
+var food160 = { name: 'Mushroom Poutine', calories: '446', cost: '$4.50' }
+var food170 = { name: 'Sweet Potato Fries', calories: '311', cost: '$2.75' }
+var food180 = { name: 'Texas BBQ Cheese Burger', calories: '764', cost: '$6.95' }
+var food190 = { name: 'Cheese Pizza', calories: '281', cost: '$2.25' }
+var food200 = { name: 'Pepperoni Pizza', calories: '367', cost: '$2.50' }
+var food210 = { name: 'Broccoli Cheddar Soup', calories: '264', cost: '$3.50' }
+var food220 = { name: 'Roots 3 Bean Chili', calories: '204', cost: '$3.50' }
+var food230 = { name: 'Fresh Garden Bar', calories: '30', cost: '$0.39' }
+var food240 = { name: 'Turkey Corn Dog', calories: '402', cost: '$2.25' }
+var food250 = { name: 'Mediterranean Rigatoni', calories: '411', cost: '$4.95' }
+var food260 = { name: 'Garlic Bread', calories: '223', cost: '$0.55' }
+//PINES UNDERNEATH
+var food1 = { name: 'Cheeseburger', calories: '579', cost: '$4.95' }
+var food2 = { name: 'Chicken Breast Sandwich', calories: '431', cost: '$5.25' }
+var food3 = { name: 'Chipotle Black Bean Burger ', calories: '465', cost: '$5.95' }
+var food4 = { name: 'Chixin Vegan Berger', calories: '736', cost: '$5.95' }
+var food5 = { name: 'Double Cheeseburger', calories: '885', cost: '$7.75' }
+var food6 = { name: 'Double Hamburger', calories: '825', cost: '$6.25' }
+var food7 = { name: 'Grilled Cheese Sandwich', calories: '675', cost: '$2.95' }
+var food8 = { name: 'Grilled Turkey Burger', calories: '515', cost: '$5.95' }
+var food9 = { name: 'Ham Bobcat Sandwich', calories: '496', cost: '$3.95' }
+var food10 = { name: 'Hamburger', calories: '528', cost: '$4.20' }
+var food11 = { name: 'Plant Based Burger', calories: '620', cost: '$5.95' }
+var food12 = { name: 'Salmon Sandwich', calories: '680', cost: '$8.25' }
+var food13 = { name: 'Crispy Onion Rings', calories: '524', cost: '$3.50' }
+var food14 = { name: 'French Fries', calories: '422', cost: '$1.75' }
+var food15 = { name: 'Fried Chicken Fritters', calories: '676', cost: '$4.50' }
+var food16 = { name: 'Mushroom Poutine', calories: '446', cost: '$4.50' }
+var food17 = { name: 'Sweet Potato Fries', calories: '311', cost: '$2.75' }
+var food18 = { name: 'Texas BBQ Cheese Burger', calories: '764', cost: '$6.95' }
+var food19 = { name: 'Cheese Pizza', calories: '281', cost: '$2.25' }
+var food20 = { name: 'Pepperoni Pizza', calories: '367', cost: '$2.50' }
+var food21 = { name: 'Broccoli Cheddar Soup', calories: '264', cost: '$3.50' }
+var food22 = { name: 'Roots 3 Bean Chili', calories: '204', cost: '$3.50' }
+var food23 = { name: 'Fresh Garden Bar', calories: '30', cost: '$0.39' }
+var food24 = { name: 'Turkey Corn Dog', calories: '402', cost: '$2.25' }
+var food25 = { name: 'Mediterranean Rigatoni', calories: '411', cost: '$4.95' }
+var food26 = { name: 'Garlic Bread', calories: '223', cost: '$0.55' }
+var ovt1 = { name: 'CYO Salad', calories: '374', cost: '$6.50' }
+var ovt2 = { name: 'Blackened Chicken', calories: '120', cost: '$3.00' }
+var ovt3 = { name: 'Blackened Salmon', calories: '109', cost: '$3.00' }
+var ovt4 = { name: 'Roasted Thai Tofu', calories: '66', cost: '$3.00' }
+var ovt5 = { name: 'Acai Bowl', calories: '381', cost: '$7.25' }
+var ovt6 = { name: 'Nutella Gone Bananas Spread', calories: '185', cost: '$1.00' }
+var ovt7 = { name: 'Pomegranate Berry Spread', calories: '132', cost: '$1.00' }
+var ovt8 = { name: 'Sundried Tomato & Spinach Spread', calories: '166', cost: '$1.00' }
+var ovt9 = { name: 'Sweet Chipotle Spread', calories: '171', cost: '$1.00' }
+var ovt10 = { name: 'Apple Kale Smoothie', calories: '343', cost: '$6.00' }
+var ovt11 = { name: 'Pomegranate Blueberry Smoothie', calories: '275', cost: '$6.00' }
+var ovt12 = { name: 'Strawberry Mango Smoothie', calories: '340', cost: '$6.00' }
+var ovt13 = { name: 'Carrot CupCake w/ Cream Cheese Icing', calories: '527', cost: '$2.00' }
+var ovt14 = { name: 'Chocolate Chip Cookie', calories: '641', cost: '$2.95' }
+var ovt15 = { name: 'Energizer Bunny Bar', calories: '287', cost: '$3.00' }
+var ovt16 = { name: 'Oatmeal Raisin BAC', calories: '625', cost: '$2.95' }
+var ovt17 = { name: 'Raspberry Cheesecake', calories: '339', cost: '$3.00' }
+var ovt18 = { name: 'S\'mores Brownie Bar', calories: '379', cost: '$3.00' }
+var ovt19 = { name: 'Turtle Cheesecake', calories: '627', cost: '$3.00' }
 
+var ovt20 = { name: 'Artichoke & Spinach', calories: '1055', cost: '$8.50' }
+var ovt21 = { name: 'Cattleman', calories: '1112', cost: '$10.50' }
+var ovt22 = { name: 'Cheese Pizza', calories: '777', cost: '$6.00' }
+var ovt23 = { name: 'Chicken Artichoke', calories: '1068', cost: '$8.50' }
+var ovt24 = { name: 'Margherita', calories: '869', cost: '$7.00' }
+var ovt25 = { name: 'Mushroom & Sausage', calories: '906', cost: '$8.50' }
+var ovt26 = { name: 'Southwest', calories: '854', cost: '$8.50' }
+var ovt27 = { name: 'The 4.0', calories: '1258', cost: '$8.50' }
+var ovt28 = { name: 'The Full Ride', calories: '1278', cost: '$10.50' }
+var ovt29 = { name: 'Vegetarian', calories: '813', cost: '$8.50' }
+
+var ovt30 = { name: 'Beef Meatball Sub', calories: '685', cost: '$6.50' }
+var ovt31 = { name: 'Fire Roasted Torrey\'s Chicken Wings', calories: '1046', cost: '$7.00' }
+var ovt32 = { name: 'Garlic Cuscinos (Pillows)', calories: '453', cost: '$2.95' }
+var ovt33 = { name: 'Cheese Pizza', calories: '2592', cost: '$14.95' }
+var ovt34 = { name: 'Margherita', calories: '2690', cost: '$15.95' }
+var ovt35 = { name: 'Pepperoni Pizza', calories: '3496', cost: '$17.95' }
+var ovt36 = { name: 'Vegetarian', calories: '2446', cost: '$17.95' }
+
+var ovt37 = { name: 'Malaysian Vindaloo Chicken', calories: '430', cost: '$6.00' }
+var ovt38 = { name: 'Swimming Rama Spicy Tofu', calories: '218', cost: '$5.00' }
+var ovt39 = { name: 'Green Beans w/ Mushrooms & Almonds', calories: '115', cost: '$2.00' }
+var ovt40 = { name: 'Nasi Tumpeng (Traditional Malaysian Rice)', calories: '215', cost: '$2.00' }
+var ovt41 = { name: 'Roasted Broccolini w/ Quinoa', calories: '121', cost: '$2.00' }
+var ovt42 = { name: 'Sauteed Edamame and Kale', calories: '126', cost: '$2.00' }
+var ovt43 = { name: 'Spicy Curry Cabbage & Potatoes', calories: '154', cost: '$2.00' }
+
+var countries = []
+
+function pickDiningHall() { 
+  let diningHall = document.title
+  if( diningHall.localeCompare( 'Canyon Vista' ) == 0 ) {
+    countries = []
+  } else if( diningHall.localeCompare( 'Goodys' ) == 0 ) {
+    countries = []
+  } else if( diningHall.localeCompare( '64 Degrees' ) == 0 ) {
+    countries = [ l64d1, l64d2, l64d3, l64d4, l64d5, l64d6, l64d7, l64d8, l64d10, l64d11, l64d12, l64d13, l64d14, l64d15, l64d16, l64d17, l64d18, l64d19, l64d20, l64d21, l64d22, l64d23, l64d24, l64d25, l64d26]
+  } else if( diningHall.localeCompare( 'Pines' ) == 0 ) {
+    countries = [ food1, food2, food3, food4, food5, food6, food7, food8, food9, food10, food11, food12, food13, food14, food15, food16, food17, food18, food19, food20, food21, food22, food23, food24, food25, food26 ]
+  } else if( diningHall.localeCompare( 'Oceanview' ) == 0 ) {
+    countries = [ ovt1, ovt2, ovt3, ovt4, ovt5, ovt6, ovt7, ovt8, ovt9, ovt10, ovt11, ovt12, ovt13, ovt14, ovt15, ovt16, ovt17, ovt18, ovt19, ovt20, ovt21, ovt22, ovt23, ovt24, ovt25, ovt26, ovt27, ovt28, ovt29, ovt30, ovt31, ovt32, ovt33, ovt34, ovt35, ovt36, ovt37, ovt38, ovt39, ovt40, ovt41, ovt42, ovt43 ]
+  } else if( diningHall.localeCompare( 'Cafe Ventanas' ) == 0 ) {
+    countries = [ lcafev1, lcafev2, lcafev3, lcafev4, lcafev5, lcafev6, lcafev7, lcafev8, lcafev9, lcafev10, lcafev12, lcafev11, lcafev13, lcafev14, lcafev15, lcafev16 ]
+  } else if( diningHall.localeCompare( 'Foodworx' ) == 0 ) {
+    countries = [ worx1, worx2, worx3, worx4, worx5, worx6, worx7, worx8, worx9, worx10, worx11, worx12, worx13, worx14, worx15, worx16, worx17, worx18, worx19, worx20, worx21, worx22, worx23, worx24, worx25, worx26, worx27 ]
+  } else if( diningHall.localeCompare( 'Roots' ) == 0 ) {
+    countries = []
+  }
+}
 function autocomplete(inp, arr) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
@@ -107,9 +295,12 @@ function autocomplete(inp, arr) {
       closeAllLists(e.target);
   });
 
+  var totalCost = 0
+  var totalCalories = 0
+
   function addToTable( name ) {
     var table = document.getElementById("myTable");
-    var row = table.insertRow(0);
+    var row = table.insertRow(1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell( 2 )
@@ -119,14 +310,19 @@ function autocomplete(inp, arr) {
         displayName = countries[ i ].name
         displayCalories = countries[ i ].calories
         displayCost = countries[ i ].cost
+        totalCalories += totalCalories
+        totalCost += displayCost
       }
     }
     cell1.innerHTML = displayName;
-    cell2.innerHTML = "Cost: " + displayCost;
-    cell3.innerHTML = "Calories: " + displayCalories
+    cell2.innerHTML = displayCalories;
+    cell3.innerHTML = displayCost
+    table.rows[ table.rows.length - 1 ].cells[ 1 ] = totalCalories
+    table.rows[ table.rows.length - 1 ].cells[ 2 ] = totalCost
   }
 }
 
 
 /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
+pickDiningHall()
 autocomplete(document.getElementById("myInput"), countries);
